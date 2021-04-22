a86ab513d5591c07e5d32b7a60cc4dbaded05107
tezcane <htezcane@gmail.com>
2021-04-22 09:16:30 -0700

    add alquran_cloud package to test android emulator network connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 //import 'package:firebase_analytics/firebase_analytics.dart';
 //import 'package:firebase_analytics/observer.dart';
+import 'package:alquran_cloud/alquran_cloud.dart' as quran_cloud;
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -14,6 +15,31 @@ import 'package:hapi/helpers/localization.g.dart';
 import 'package:hapi/ui/components/loading.dart';
 
 void main() async {
+  // TODO REMOVE THIS CODE LATER BUT USED TO DETECT ANDROID EMULATOR FAILING NETWORK CONNECTIONS
+  /// to enable logs (disabled by default)
+  quran_cloud.quranCloud.enableLogs = true;
+
+  /// use edition identifer to determine which edition of the quran to get
+  final allEditions = await quran_cloud.getAllEditions();
+
+  // /// also you can query the edition you want
+  // final editionsQuery = await quran_cloud.getAllEditions(
+  //   format: 'text', // or `audio`
+  //   language:
+  //       'ar', // use .getEditionSupportedLanguages(); to get the all available languages
+  //   type: 'quran', // user .getEditionTypes() to get all available types
+  // );
+
+  // final quran = await quran_cloud.getQuranByEdition(allEditions.first);
+
+  // /// get surah by number and edition
+  // final surah = await quran_cloud.getSurahByEdition(1, editionsQuery.first);
+
+  // /// get aya by number and edition
+  // final aya = await quran_cloud.getAyaByNumber(2, editionsQuery.first);
+
+  // print(aya.text); // الْحَمْدُ لِلَّهِ رَبِّ الْعَالَمِينَ
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   await GetStorage.init();
