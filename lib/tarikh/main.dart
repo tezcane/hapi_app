c226d561c030d45a9ae884116d7c440eb630689b
tezcane <htezcane@gmail.com>
2021-04-29 22:05:06 -0700

    add tarikh assets, fonts and it's own main to launch outside of hapi app

---
+++ lib/tarikh/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hapi/tarikh/blocs/bloc_provider.dart';
+import 'package:hapi/tarikh/colors.dart';
+import 'package:hapi/tarikh/main_menu/main_menu.dart';
+
+/// The app is wrapped by a [BlocProvider]. This allows the child widgets
+/// to access other components throughout the hierarchy without the need
+/// to pass those references around.
+class TimelineApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    return BlocProvider(
+      child: MaterialApp(
+        title: 'History & Future of Everything',
+        theme: ThemeData(
+            backgroundColor: background, scaffoldBackgroundColor: background),
+        home: MenuPage(),
+      ),
+      platform: Theme.of(context).platform,
+    );
+  }
+}
+
+class MenuPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: null, body: MainMenuWidget());
+  }
+}
+
+void main() => runApp(TimelineApp()); // TODO we can delete this later
