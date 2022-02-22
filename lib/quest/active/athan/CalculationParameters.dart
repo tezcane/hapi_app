9ec67b9d8a6d99da5e8e16f30fc1b22d9990254e
tezcane <htezcane@gmail.com>
2022-02-22 15:08:37 -0800

    Cleanup warnings

--- lib/quest/active/athan/CalculationParameters.dart
+++ lib/quest/active/athan/CalculationParameters.dart
@@ -40,15 +40,15 @@ class CalculationParameters {
   });
 
   nightPortions() {
-    switch (this.highLatitudeRule) {
+    switch (highLatitudeRule) {
       case HighLatitudeRule.MiddleOfTheNight:
         return {'fajr': 1 / 2, 'isha': 1 / 2};
       case HighLatitudeRule.SeventhOfTheNight:
         return {'fajr': 1 / 7, 'isha': 1 / 7};
       case HighLatitudeRule.TwilightAngle:
-        return {'fajr': this.fajrAngle / 60, 'isha': this.ishaAngle / 60};
+        return {'fajr': fajrAngle / 60, 'isha': ishaAngle / 60};
       default:
-        throw ('Invalid high latitude rule found when attempting to compute night portions: ${this.highLatitudeRule}');
+        throw 'Invalid high latitude rule found when attempting to compute night portions: $highLatitudeRule';
     }
   }
 }
