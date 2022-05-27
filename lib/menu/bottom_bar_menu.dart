f30f2c149ac013486dee16fcb69aa5b11898b98d
tezcane <htezcane@gmail.com>
2022-05-27 12:31:53 -0700

    Fix bug where only Tarikh Menu->Favorites transition by tapping BottomBar would stop in middle Search page. Strangely, Acitve Questes Bottom Bar with 4 items (instead of Tarikh's 3 items), worked fine in all directions.

--- lib/menu/bottom_bar_menu.dart
+++ lib/menu/bottom_bar_menu.dart
@@ -5,14 +5,17 @@ import 'package:hapi/menu/bottom_bar.dart';
 import 'package:hapi/menu/menu_controller.dart';
 
 /// Controls NavPage bottom bars and loads/persists new tab selections.
+// ignore: must_be_immutable
 class BottomBarMenu extends StatelessWidget {
   BottomBarMenu(this.navPage, this.bottomBarItems, this.aliveMainWidgets);
   final NavPage navPage;
   final List<BottomBarItem> bottomBarItems;
   final List<Widget> aliveMainWidgets;
 
-  late PageController _pageController; // TODO make BottomBarMenu const
+  late PageController _pageController;
   bool initNeeded = true;
+  int navMovingToIdx = -1;
+  int curBottomBarHighlightIdx = 0; // to briefly highlight bottom bar indexes
 
   @override
   Widget build(BuildContext context) {
@@ -32,12 +35,13 @@ class BottomBarMenu extends StatelessWidget {
         bottomNavigationBar: BottomBar(
           // Disable to turn off bottom bar view, so menu blends to page:
           //backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-          selectedIndex: c.getLastIdx(navPage),
+          selectedIndex: curBottomBarHighlightIdx,
           items: bottomBarItems,
           height: 40,
           onTap: (newIdx) => _onBottomBarTabTapped(newIdx),
+          // Disable to turn off bottom bar view, so menu blends to page:
+          //backgroundColor: Theme.of(context).scaffoldBackgroundColor, null
           //showActiveBackgroundColor: false,
-          //backgroundColor: null,
         ),
       );
     });
@@ -45,38 +49,44 @@ class BottomBarMenu extends StatelessWidget {
 
   /// Called at init only
   _initPageControllerAndBottomBar(int newIdx) {
+    curBottomBarHighlightIdx = newIdx;
     _pageController = PageController(initialPage: newIdx);
-    _handlePostFrameAnimation(newIdx, jumpToPage: true); // needed for RTL<->LTR
+    _handlePostFrameAnimation(newIdx); // needed for RTL<->LTR
   }
 
   /// Called directly when swiping page or indirectly on bottom bar tab tap
   _onPageChanged(int newIdx) {
-    // Optional callback, can enable/disable a tab page's animations, etc.
-    if (bottomBarItems[newIdx].onPressed != null) {
-      bottomBarItems[newIdx].onPressed!.call();
+    curBottomBarHighlightIdx = newIdx; // always highlight current page
+
+    // only do below work when we get final animated page or if swiped to page
+    if (navMovingToIdx == newIdx || navMovingToIdx == -1) {
+      if (navMovingToIdx == newIdx) navMovingToIdx = -1;
+      // Optional callback, can enable/disable a tab page's animations, etc.
+      if (bottomBarItems[newIdx].onPressed != null) {
+        bottomBarItems[newIdx].onPressed!.call();
+      }
+      NavPageController.to.setLastIdx(navPage, newIdx);
+    } else {
+      NavPageController.to.update(); // to flash intermediate bottom bar select
     }
-    NavPageController.to.setLastIdx(navPage, newIdx);
   }
 
   /// Called only when bottom bar tab is tapped
   _onBottomBarTabTapped(int newIdx) {
     if (newIdx == NavPageController.to.getLastIdx(navPage)) return; // we r here
+    navMovingToIdx = newIdx;
     _handlePostFrameAnimation(newIdx); // needed for stateless widget to work
   }
 
   /// Called at init and when bottom bar tab is pressed, must do in
   /// PostFrameCallback so all needed objects are initialized.
-  _handlePostFrameAnimation(int newIdx, {bool jumpToPage = false}) {
+  _handlePostFrameAnimation(int newIdx) {
     WidgetsBinding.instance!.addPostFrameCallback((_) {
-      if (jumpToPage) {
-        _pageController.jumpToPage(newIdx); // boring jump to page at init
-      } else {
-        _pageController.animateToPage(
-          newIdx,
-          curve: Curves.easeInOut,
-          duration: const Duration(milliseconds: 750),
-        ); // this animates to the page on tab press
-      }
+      _pageController.animateToPage(
+        newIdx,
+        curve: Curves.easeInOut,
+        duration: const Duration(milliseconds: 750),
+      ); // this animates to the page on tab press
       // jumpToPage/animateToPage triggers onPageChanged, so don't need:
       //NavPageController.to.setLastIdx(widget.navPage, newIdx);
     });
