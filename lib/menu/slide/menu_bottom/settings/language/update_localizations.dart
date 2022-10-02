2d0a193d98813ab4130e47ece6d98283c7832452
tezcane <htezcane@gmail.com>
2022-10-02 00:35:42 -0700

    Fix translations, filters "r.<key>" to it's own file now, like how "t.<key>" works. "t." and "r." no longer are in each key in their respected translation files.

--- lib/menu/slide/menu_bottom/settings/language/update_localizations.dart
+++ lib/menu/slide/menu_bottom/settings/language/update_localizations.dart
@@ -21,12 +21,13 @@ void main() async {
   stdout.writeln('---------------------------------------');
 
   String phraseKey = '';
-  List<LocalizationModel> localizations = [];
+  List<Locale> localizations = [];
 
   try {
-    var response = await http
-        .get(Uri.parse(url), headers: {'accept': 'text/csv;charset=UTF-8'});
-
+    var response = await http.get(
+      Uri.parse(url),
+      headers: {'accept': 'text/csv;charset=UTF-8'},
+    );
     // print('Google sheet csv:\n ${response.body}');
 
     final bytes = response.bodyBytes.toList();
@@ -69,16 +70,16 @@ void main() async {
         } else {
           bool languageAdded = false;
           for (var element in localizations) {
-            if (element.language == key) {
-              element.phrases.add(PhraseModel(key: phraseKey, phrase: value));
+            if (element.lang == key) {
+              element.phrases.add(Phrase(phraseKey, value));
               languageAdded = true;
             }
           }
           if (languageAdded == false) {
             localizations.add(
-              LocalizationModel(
-                language: key,
-                phrases: [PhraseModel(key: phraseKey, phrase: value)],
+              Locale(
+                lang: key,
+                phrases: [Phrase(phraseKey, value)],
               ),
             );
           }
@@ -86,10 +87,12 @@ void main() async {
       });
     }
 
-    //_updateLocalizationFile(localizations); // obsolete, uses too much memory
-    await _updateTranslationFiles(localizations, true, false);
-    await _updateTranslationFiles(localizations, true, true);
-    await _updateTranslationFiles(localizations, false, false);
+    // updateLocalizationFile(localizations); // obsolete, uses too much memory
+
+    await _updateMainTranslationFiles(localizations, ['t.', 'r.']);
+    await _updateArabicOnlyFile(localizations);
+    await _updateBigTranslationFiles(localizations, 't.', 't/');
+    await _updateBigTranslationFiles(localizations, 'r.', 'r/');
   } catch (e) {
     //output error
     stderr.writeln('error: networking error');
@@ -97,47 +100,44 @@ void main() async {
   }
 }
 
-/// Creates multiple json files with all translations. App must load each one
-/// when the language preference updates. We filter out "t." tarikh articles if
-/// filterTarikhArticles is true, otherwise add them to $path.
-Future _updateTranslationFiles(
-  List<LocalizationModel> localizations,
-  bool filterTarikhArticles,
-  bool createArabicFile,
+/// Creates multiple json files with all translations per language. It saves all
+/// translations except those specified in the keysToFilterOut list. Those need
+/// to be written out using _updateBigTranslationFiles().  Note, it does write
+/// the "a.<key>" files because these are the local language translations needed
+/// to show the arabic words in the user's native language (if non-arabic). The
+/// keys for "a." keys is the Arabic transliteration as well, so this is used as
+/// a stepping stone to learn arabic script and the Arabic word.
+Future _updateMainTranslationFiles(
+  List<Locale> localizations,
+  List<String> keysToFilterOut,
 ) async {
   int count = 0;
   for (var localization in localizations) {
-    if (createArabicFile && localization.language != 'ar') continue;
-
     count++;
     String text = '{';
     for (var phrase in localization.phrases) {
       if (phrase.key.startsWith('dummy.')) continue;
 
-      if (filterTarikhArticles) {
-        if (phrase.key.startsWith('t.')) continue;
-      } else {
-        if (!createArabicFile) {
-          if (!phrase.key.startsWith('t.')) continue;
-        }
+      bool keyToFilterOutFound = false;
+      for (String keyToFilterOut in keysToFilterOut) {
+        if (phrase.key.startsWith(keyToFilterOut)) keyToFilterOutFound = true;
       }
+      if (keyToFilterOutFound) continue;
 
-      if (createArabicFile && !phrase.key.startsWith('a.')) continue;
+      String key = phrase.key
+          .replaceAll(r'"', '\\"') // escape quotes for json syntax
+          .replaceAll('\n', '\\n'); // escape new lines for json syntax
 
-      String phraseKey =
-          phrase.key.replaceAll(r'"', '\\"').replaceAll('\n', '\\n');
-      String phrasePhrase =
-          phrase.phrase.replaceAll(r'"', '\\"').replaceAll('\n', '\\n');
-      String currentPhraseTextCode = '\n"$phraseKey": "$phrasePhrase",';
+      String value = phrase.value
+          .replaceAll(r'"', '\\"') // escape quotes for json syntax
+          .replaceAll('\n', '\\n'); // escape new lines for json syntax
+      String currentPhraseTextCode = '\n"$key": "$value",';
       text += currentPhraseTextCode;
     }
-    // Remove last comma
-    text = text.substring(0, text.length - 1);
+    text = text.substring(0, text.length - 1); // Remove last comma
     text += '\n}\n';
 
-    String path = filterTarikhArticles ? '' : 'tarikh_articles/';
-    String filePrefix = !createArabicFile ? localization.language : 'a';
-    String filename = '../../../../../../assets/i18n/$path$filePrefix.json';
+    String filename = '../../../../../../assets/i18n/${localization.lang}.json';
     stdout.writeln('Saving $filename');
     final file = File(filename);
     await file.writeAsString(text);
@@ -145,41 +145,78 @@ Future _updateTranslationFiles(
   stdout.writeln('Done writing $count json files');
 }
 
-/// Creates a single dart file with all translations, ok if your app is small
-/// but if you have lots of translation it takes up too much memory.
-Future updateLocalizationFile(List<LocalizationModel> localizations) async {
-  String localizationFile = """import 'package:get/get.dart';
-
-class Localization extends Translations {
-  @override
-  Map<String, Map<String, String>> get keys => {
-    """;
-
+/// Creates a single a.json file with all arabic translations in it. These are
+/// the key to teaching the user Arabic.  They are always loaded in memory.
+Future _updateArabicOnlyFile(List<Locale> localizations) async {
+  int count = 0;
   for (var localization in localizations) {
-    String language = localization.language;
-    localizationFile += "'$language': {\n";
-    for (var phrase in localization.phrases) {
-      String phraseKey =
-          phrase.key.replaceAll(r"'", "\\'").replaceAll('\n', '\\n');
-      String phrasePhrase =
-          phrase.phrase.replaceAll(r"'", "\\'").replaceAll('\n', '\\n');
-      String currentPhraseTextCode = "'$phraseKey': '$phrasePhrase',\n";
-      localizationFile += currentPhraseTextCode;
-    }
-    localizationFile += '},\n';
-  }
-  localizationFile += '''
-  };
-}
-''';
+    if (localization.lang != 'ar') continue;
 
-  stdout.writeln('');
-  stdout.writeln('---------------------------------------');
-  stdout.writeln('Saving localization.g.dart');
-  stdout.writeln('---------------------------------------');
-  final file = File('localization.g.dart');
-  await file.writeAsString(localizationFile);
-  stdout.writeln('Done...');
+    count++;
+    String text = '{';
+    for (var phrase in localization.phrases) {
+      if (!phrase.key.startsWith('a.')) continue;
+//    if (phrase.key.startsWith('dummy.')) continue;
+
+      String key = phrase.key
+          .replaceFirst('a.', '') // remove key tag from out file
+          .replaceAll(r'"', '\\"') // escape quotes for json syntax
+          .replaceAll('\n', '\\n'); // escape new lines for json syntax
+
+      String value = phrase.value
+          .replaceAll(r'"', '\\"') // escape quotes for json syntax
+          .replaceAll('\n', '\\n'); // escape new lines for json syntax
+      String currentPhraseTextCode = '\n"$key": "$value",';
+      text += currentPhraseTextCode;
+    }
+    text = text.substring(0, text.length - 1); // Remove last comma
+    text += '\n}\n';
+
+    String filename = '../../../../../../assets/i18n/a.json';
+    stdout.writeln('Saving $filename');
+    final file = File(filename);
+    await file.writeAsString(text);
+  }
+  stdout.writeln('Done writing $count json files');
+}
+
+/// Filters "t.<key>" / "r.<key>" tarikh, relic, etc. big text descriptions into
+/// their own folder path.  These are to save memory and not always have this
+/// data loaded.
+Future _updateBigTranslationFiles(
+  List<Locale> localizations,
+  String keyToFilterFor,
+  String outFolder,
+) async {
+  int count = 0;
+  for (var localization in localizations) {
+    count++;
+    String text = '{';
+    for (var phrase in localization.phrases) {
+      if (!phrase.key.startsWith(keyToFilterFor)) continue;
+//    if (phrase.key.startsWith('dummy.')) continue;
+
+      String key = phrase.key
+          .replaceFirst(keyToFilterFor, '') // remove key tag from out file
+          .replaceAll(r'"', '\\"') // escape quotes for json syntax
+          .replaceAll('\n', '\\n'); // escape new lines for json syntax
+
+      String value = phrase.value
+          .replaceAll(r'"', '\\"') // escape quotes for json syntax
+          .replaceAll('\n', '\\n'); // escape new lines for json syntax
+      String currentPhraseTextCode = '\n"$key": "$value",';
+      text += currentPhraseTextCode;
+    }
+    text = text.substring(0, text.length - 1); // Remove last comma
+    text += '\n}\n';
+
+    String filename =
+        '../../../../../../assets/i18n/$outFolder${localization.lang}.json';
+    stdout.writeln('Saving $filename');
+    final file = File(filename);
+    await file.writeAsString(text);
+  }
+  stdout.writeln('Done writing $count json files');
 }
 
 String _uniformizeKey(String key) {
@@ -188,43 +225,32 @@ String _uniformizeKey(String key) {
 }
 
 //Localization Model
-class LocalizationModel {
-  final String language;
-  final List<PhraseModel> phrases;
-
-  LocalizationModel({
-    required this.language,
+class Locale {
+  Locale({
+    required this.lang,
     required this.phrases,
   });
+  final String lang;
+  final List<Phrase> phrases;
 
-  factory LocalizationModel.fromMap(Map data) {
-    return LocalizationModel(
-      language: data['language'],
-      phrases:
-          (data['phrases'] as List).map((v) => PhraseModel.fromMap(v)).toList(),
-    );
-  }
+  factory Locale.fromMap(Map data) => Locale(
+        lang: data['lang'],
+        phrases:
+            (data['phrases'] as List).map((v) => Phrase.fromMap(v)).toList(),
+      );
 
   Map<String, dynamic> toJson() => {
-        'language': language,
+        'lang': lang,
         'phrases': List<dynamic>.from(phrases.map((x) => x.toJson())),
       };
 }
 
-class PhraseModel {
-  String key;
-  String phrase;
+class Phrase {
+  Phrase(this.key, this.value);
+  final String key;
+  final String value;
 
-  PhraseModel({required this.key, required this.phrase});
+  factory Phrase.fromMap(Map data) => Phrase(data['key'], data['phrase']);
 
-  factory PhraseModel.fromMap(Map data) {
-    return PhraseModel(
-      key: data['key'],
-      phrase: data['phrase'] ?? '',
-    );
-  }
-  Map<String, dynamic> toJson() => {
-        'key': key,
-        'phrase': phrase,
-      };
+  Map<String, dynamic> toJson() => {'key': key, 'phrase': value};
 }
